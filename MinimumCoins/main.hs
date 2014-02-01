937885a53a91fd33ef3a6f3a704b921d2b741245
Carsten König <Carsten@gettingsharper.de>
2014-02-01 13:05:56 +0100

    parse / show for the MinimumCoin problem

--- MinimumCoins/main.hs
+++ MinimumCoins/main.hs
@@ -1,19 +1,29 @@
 module Main where
 
-import Control.Monad (forM_)
-import Data.List (sort)
+import Control.Applicative ((<$>))
+import Data.List (sort, delete)
 
-type Problem = ()
-type Solution = ()
+type Rupee    = Int
+type Amount   = Int
+data Problem  = Problem  { amount :: Amount, rupees :: [Rupee]}
+data Solution = Solution [(Rupee, Int)]
 
 main :: IO ()
 main = getProblem >>= printSolution . solveProblem
 
 getProblem :: IO Problem
-getProblem = undefined
+getProblem = do
+  am <- readLn
+  rs <- sort . read . curly2brace <$> getLine
+  return $ Problem am rs
+  where curly2brace :: String -> String
+        curly2brace = ('[':) . (++"]") . delete '{' . delete '}'
 
 solveProblem :: Problem -> Solution
 solveProblem _ = undefined
 
 printSolution :: Solution -> IO ()
-printSolution = undefined
+printSolution = putStrLn . show
+
+instance Show Solution where
+  show (Solution ss) = unlines . map (\(r,n) -> "Rupee " ++ show r ++ " coin " ++ show n) $ ss
